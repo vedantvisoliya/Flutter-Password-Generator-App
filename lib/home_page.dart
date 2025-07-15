0997a154d276d2b96542f37557a937662296582c
Vedant Kumar <vedantvisoliya@gmail.com>
2025-07-16 01:19:42 +0530

    added responsiveness

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -37,21 +37,25 @@ class _HomePageState extends State<HomePage> {
         // height and width.
         width: screenWidth,
         height: screenHeight,
-        
+
         // background colors blend of 3.
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color.fromARGB(255 ,214, 196, 242), Color.fromARGB(255, 178, 141, 255), Color.fromARGB(255, 138, 77, 255)],
+            colors: [
+              Color.fromARGB(255, 214, 196, 242),
+              Color.fromARGB(255, 178, 141, 255),
+              Color.fromARGB(255, 138, 77, 255),
+            ],
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
-          )
+          ),
         ),
 
         // container
         child: Center(
           child: Container(
-            width: screenWidth/2 + 100,
-            height: screenHeight/2 + 50, 
+            width: 700,
+            height: 330,
             // glass like transparency
             decoration: BoxDecoration(
               color: Colors.white.withValues(
@@ -76,7 +80,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
 
-                    const SizedBox(height: 5,),
+                    const SizedBox(height: 5),
 
                     // SUBTITLE ------>
                     Column(
@@ -100,14 +104,16 @@ class _HomePageState extends State<HomePage> {
                       ],
                     ),
 
-                    const SizedBox(height: 10,),
+                    const SizedBox(height: 10),
 
                     // DISPLAY AND BUTTON ------->
                     Row(
                       children: [
                         Expanded(
                           child: Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 10.0,
+                            ),
                             decoration: BoxDecoration(
                               color: Colors.white.withValues(
                                 alpha: 0.27,
@@ -129,10 +135,13 @@ class _HomePageState extends State<HomePage> {
                                     fontWeight: FontWeight.w600,
                                   ),
                                 ),
-                            
+
                                 // copy icon
                                 IconButton(
-                                  onPressed: () => generate.copyText(context, _genratedPassword ?? "Create Now."),
+                                  onPressed: () => generate.copyText(
+                                    context,
+                                    _genratedPassword ?? "Create Now.",
+                                  ),
                                   icon: Icon(
                                     Icons.copy,
                                     color: Colors.grey.shade500,
@@ -143,12 +152,15 @@ class _HomePageState extends State<HomePage> {
                             ),
                           ),
                         ),
- 
-                        const SizedBox(width: 10.0,),
+
+                        const SizedBox(width: 10.0),
 
                         // generate password button
                         Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25.0,
+                            vertical: 10.0,
+                          ),
                           decoration: BoxDecoration(
                             color: Color.fromARGB(255, 34, 35, 59),
                             borderRadius: BorderRadius.circular(5.0),
@@ -158,20 +170,26 @@ class _HomePageState extends State<HomePage> {
                               onTap: () {
                                 if (lengthController.text.isNotEmpty) {
                                   setState(() {
-                                    _genratedPassword = generate.generatePassword(
-                                      passwordLength: int.parse(lengthController.text), 
-                                      isNumber: allowNumbers, 
-                                      isUpperCase: allowUpperCase, 
-                                      isLowerCase: allowLowercase, 
-                                      isSymbol: allowSymbols, 
-                                      isDuplicates: allowDuplicates, 
-                                      context: context
-                                    );
+                                    _genratedPassword = generate
+                                        .generatePassword(
+                                          passwordLength: int.parse(
+                                            lengthController.text,
+                                          ),
+                                          isNumber: allowNumbers,
+                                          isUpperCase: allowUpperCase,
+                                          isLowerCase: allowLowercase,
+                                          isSymbol: allowSymbols,
+                                          isDuplicates: allowDuplicates,
+                                          context: context,
+                                        );
                                   });
-                                }
-                                else {
+                                } else {
                                   ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(content: Text("Error: Enter password length between 6 and 32.")),
+                                    SnackBar(
+                                      content: Text(
+                                        "Error: Enter password length between 6 and 32.",
+                                      ),
+                                    ),
                                   );
                                 }
                               },
@@ -180,7 +198,7 @@ class _HomePageState extends State<HomePage> {
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.w600,
-                                ), 
+                                ),
                               ),
                             ),
                           ),
@@ -188,7 +206,7 @@ class _HomePageState extends State<HomePage> {
                       ],
                     ),
 
-                    const SizedBox(height: 20.0,),
+                    const SizedBox(height: 20.0),
 
                     // CUSTOMISE PASSWORD ------>
                     Expanded(
@@ -202,7 +220,7 @@ class _HomePageState extends State<HomePage> {
                             blue: 255,
                           ),
                           borderRadius: BorderRadius.circular(5.0),
-                          ), 
+                        ),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
@@ -224,7 +242,7 @@ class _HomePageState extends State<HomePage> {
                                       ),
                                     ),
 
-                                    const SizedBox(width: 10.0,),
+                                    const SizedBox(width: 10.0),
 
                                     // text field
                                     SizedBox(
@@ -238,23 +256,29 @@ class _HomePageState extends State<HomePage> {
                                         maxLength: 2,
                                         keyboardType: TextInputType.number,
                                         controller: lengthController,
-                                        decoration: InputDecoration( 
-                                          contentPadding: EdgeInsets.only(bottom: 5.0),
+                                        decoration: InputDecoration(
+                                          contentPadding: EdgeInsets.only(
+                                            bottom: 5.0,
+                                          ),
                                           counterText: "",
                                           filled: true,
                                           fillColor: Colors.black,
                                           border: OutlineInputBorder(
-                                            borderRadius: BorderRadius.circular(4.0), // Adjust value for more/less curve
-                                            borderSide: BorderSide(color: Colors.black),
+                                            borderRadius: BorderRadius.circular(
+                                              4.0,
+                                            ), // Adjust value for more/less curve
+                                            borderSide: BorderSide(
+                                              color: Colors.black,
+                                            ),
                                           ),
-                                        ), 
+                                        ),
                                         textAlign: TextAlign.center,
                                       ),
                                     ),
                                   ],
                                 ),
 
-                                const SizedBox(height: 10.0,),
+                                const SizedBox(height: 10.0),
 
                                 // allow numbers
                                 Row(
@@ -270,10 +294,10 @@ class _HomePageState extends State<HomePage> {
                                     ),
                                     // check box
                                     Checkbox(
-                                      value: allowNumbers, 
+                                      value: allowNumbers,
                                       onChanged: (bool? newValue) {
-                                        setState(() {    
-                                          allowNumbers = newValue!; 
+                                        setState(() {
+                                          allowNumbers = newValue!;
                                         });
                                       },
                                       activeColor: Colors.black,
@@ -284,7 +308,7 @@ class _HomePageState extends State<HomePage> {
                                   ],
                                 ),
 
-                                const SizedBox(height: 10.0,),
+                                const SizedBox(height: 10.0),
 
                                 // allow uppercase letters
                                 Row(
@@ -300,10 +324,10 @@ class _HomePageState extends State<HomePage> {
                                     ),
                                     // check box
                                     Checkbox(
-                                      value: allowUpperCase, 
+                                      value: allowUpperCase,
                                       onChanged: (bool? newValue) {
-                                        setState(() {    
-                                          allowUpperCase = newValue!; 
+                                        setState(() {
+                                          allowUpperCase = newValue!;
                                         });
                                       },
                                       activeColor: Colors.black,
@@ -335,10 +359,10 @@ class _HomePageState extends State<HomePage> {
                                     ),
                                     // check box
                                     Checkbox(
-                                      value: allowLowercase, 
+                                      value: allowLowercase,
                                       onChanged: (bool? newValue) {
-                                        setState(() {    
-                                          allowLowercase = newValue!; 
+                                        setState(() {
+                                          allowLowercase = newValue!;
                                         });
                                       },
                                       activeColor: Colors.black,
@@ -349,7 +373,7 @@ class _HomePageState extends State<HomePage> {
                                   ],
                                 ),
 
-                                const SizedBox(height: 10.0,),
+                                const SizedBox(height: 10.0),
 
                                 // allow symbols
                                 Row(
@@ -365,10 +389,10 @@ class _HomePageState extends State<HomePage> {
                                     ),
                                     // check box
                                     Checkbox(
-                                      value: allowSymbols, 
+                                      value: allowSymbols,
                                       onChanged: (bool? newValue) {
-                                        setState(() {    
-                                          allowSymbols = newValue!; 
+                                        setState(() {
+                                          allowSymbols = newValue!;
                                         });
                                       },
                                       activeColor: Colors.black,
@@ -379,7 +403,7 @@ class _HomePageState extends State<HomePage> {
                                   ],
                                 ),
 
-                                const SizedBox(height: 10.0,),
+                                const SizedBox(height: 10.0),
 
                                 // allow duplicates
                                 Row(
@@ -395,10 +419,10 @@ class _HomePageState extends State<HomePage> {
                                     ),
                                     // check box
                                     Checkbox(
-                                      value: allowDuplicates, 
+                                      value: allowDuplicates,
                                       onChanged: (bool? newValue) {
-                                        setState(() {    
-                                          allowDuplicates = newValue!; 
+                                        setState(() {
+                                          allowDuplicates = newValue!;
                                         });
                                       },
                                       activeColor: Colors.black,
@@ -409,7 +433,7 @@ class _HomePageState extends State<HomePage> {
                                   ],
                                 ),
                               ],
-                            )
+                            ),
                           ],
                         ),
                       ),
